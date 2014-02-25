d8c7e68040551cedb9dc39f91e186e643bbf89b3
MDMunir <misbahuldmunir@gmail.com>
2014-02-25 00:11:41 -0800

    index.php

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -1,5 +1,4 @@
 <?php
-
 use backend\assets\AppAsset;
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
@@ -17,16 +16,16 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-	<head>
-		<meta charset="<?= Yii::$app->charset ?>"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title><?= Html::encode($this->title) ?></title>
-		<?php $this->head() ?>
-	</head>
-	<body>
-		<?php $this->beginBody() ?>
-		<div class="wrap">
-			<?php
+<head>
+	<meta charset="<?= Yii::$app->charset ?>"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?= Html::encode($this->title) ?></title>
+	<?php $this->head() ?>
+</head>
+<body>
+	<?php $this->beginBody() ?>
+	<div class="wrap">
+		<?php
 			NavBar::begin([
 				'brandLabel' => 'My Company',
 				'brandUrl' => Yii::$app->homeUrl,
@@ -40,62 +39,31 @@ AppAsset::register($this);
 			if (Yii::$app->user->isGuest) {
 				$menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
 			} else {
-				$menuItems[] = ['label' => 'Logout (' . Yii::$app->user->identity->username . ')', 'url' => ['/site/logout']];
+				$menuItems[] = ['label' => 'Logout (' . Yii::$app->user->identity->username .')' , 'url' => ['/site/logout']];
 			}
-			$menuItems[] = ['label' => '<span class="glyphicon glyphicon-th"></span>',
-				'items' => Dropdown::widget([
-					'items' => [
-						'<button id="cw3" class="btn"><span class="glyphicon glyphicon-align-justify"></span></button>
-<button id="cw4" class="btn"><span class="glyphicon glyphicon-align-justify"></span></button>
-<button id="cw5" class="btn"><span class="glyphicon glyphicon-align-justify"></span></button>
-<button id="cw6" class="btn"><span class="glyphicon glyphicon-align-justify"></span></button>
-<button id="cw7" class="btn"><span class="glyphicon glyphicon-align-justify"></span></button>
-<button id="cw8" class="btn"><span class="glyphicon glyphicon-align-justify"></span></button>
-<button id="cw9" class="btn"><span class="glyphicon glyphicon-align-justify"></span></button>
-<button id="cw10" class="btn"><span class="glyphicon glyphicon-align-justify"></span></button>
-<button id="cw11" class="btn"><span class="glyphicon glyphicon-align-justify"></span></button>
-<button id="cw12" class="btn"><span class="glyphicon glyphicon-align-justify"></span></button>',
-					],
-					'encodeLabels' => true,
-					'clientOptions' => false,
-					'options' => ['style' => 'right: -60px;min-width: 200px;'],
-					'view' => $this,
-				])
-			];
 			echo Nav::widget([
 				'options' => ['class' => 'navbar-nav navbar-right'],
-				'encodeLabels' => false,
 				'items' => $menuItems,
 			]);
-			echo Nav::widget([
-				'options' => ['class' => 'navbar-nav navbar-left'],
-				'items' => [
-					['label' => 'm1'],
-					['label' => 'm2'],
-					['label' => 'm3'],
-				]
-			]);
 			NavBar::end();
-			?>
+		?>
 
-			<div class="container">
-				<?=
-				Breadcrumbs::widget([
-					'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-				])
-				?>
-				<?= $content ?>
-			</div>
+		<div class="container">
+		<?= Breadcrumbs::widget([
+			'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+		]) ?>
+		<?= $content ?>
 		</div>
+	</div>
 
-		<footer class="footer">
-			<div class="container">
-				<p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-				<p class="pull-right"><?= Yii::powered() ?></p>
-			</div>
-		</footer>
+	<footer class="footer">
+		<div class="container">
+		<p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+		<p class="pull-right"><?= Yii::powered() ?></p>
+		</div>
+	</footer>
 
-		<?php $this->endBody() ?>
-	</body>
+	<?php $this->endBody() ?>
+</body>
 </html>
 <?php $this->endPage() ?>
