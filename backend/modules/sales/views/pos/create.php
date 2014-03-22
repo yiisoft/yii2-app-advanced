6788936c2e92005c6b34c2386eb7240386d7ea66
sangkilsoft <masyhudi.melik@gmail.com>
2014-03-22 21:26:22 +0700

    di bedakin

--- backend/modules/sales/views/pos/create.php
+++ backend/modules/sales/views/pos/create.php
@@ -6,18 +6,16 @@ use yii\helpers\Html;
  * @var yii\web\View $this
  * @var backend\modules\sales\models\SalesHdr $model
  */
-
 $this->title = 'Create Sales Hdr';
 $this->params['breadcrumbs'][] = ['label' => 'Sales Hdrs', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="sales-hdr-create">
-
-	<h1><?= Html::encode($this->title) ?></h1>
-
-	<?php echo $this->render('_form', [
-		'model' => $model,
-		'detailProvider'=>$detailProvider,
-	]); ?>
-
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php
+    echo $this->render('_form', [
+        'model' => $model,
+        'detailProvider' => $detailProvider,
+    ]);
+    ?>
 </div>
