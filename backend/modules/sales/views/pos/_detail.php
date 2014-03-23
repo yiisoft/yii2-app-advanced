4b2d22ed737dcf7d84da9d97a67dd7f5163e5cfe
sangkilsoft <masyhudi.melik@gmail.com>
2014-03-23 18:22:35 +0700

    di bedakin

--- backend/modules/sales/views/pos/_detail.php
+++ backend/modules/sales/views/pos/_detail.php
@@ -4,50 +4,52 @@ use yii\web\JsExpression;
 use yii\jui\AutoComplete;
 ?>
 <div class="panel panel-default">
-	<div class="panel-body pull-right">
-		Product :
-		<?php
-		echo AutoComplete::widget([
-			'name' => 'product',
-			'id' => 'product',
-			'clientOptions' => [
-				'source' => new JsExpression('yii.pos.sourceProduct'),
-				'select' => new JsExpression('yii.pos.onSelectProduct'),
-				'delay' => 500
-			]
-		]);
-		?>
-		<input type="hidden" data-field="total_price"><span id="total-price"></span>
-	</div>
-	<table id="detail-grid" class="table table-striped" style="padding: 0px;">
-		<thead style="display: none">
-			<tr>
-				<td style="width: 50px">
-					<a data-action="delete" title="Delete" href="#"><span class="glyphicon glyphicon-trash"></span></a>
-					<input type="hidden" data-field="price"><input type="hidden" data-field="id_uom">
-					<input type="hidden" data-field="id_product">
-				</td>
-				<td>
-					<ul class="nav nav-list">
-						<li class="item">
-							<span data-text="nm_product"></span>
-						</li>
-						<li class="qty">
-							Jumlah <input type="text" size="5" data-field="qty" value="1"> <span data-text="nm_uom"></span>
-							@ Rp<span data-text="price"></span>
-						</li>
-						<li class="discon">
-							Discon <input type="text" size="5" data-field="discon"> %
+    <div class="panel-heading">
+        Product :
+        <?php
+        echo AutoComplete::widget([
+            'name' => 'product',
+            'id' => 'product',
+            'clientOptions' => [
+                'source' => new JsExpression('yii.pos.sourceProduct'),
+                'select' => new JsExpression('yii.pos.onSelectProduct'),
+                'delay' => 500
+            ]
+        ]);
+        ?>
+    </div>
+    <div class="panel-body pull-right">        
+        <input type="hidden" data-field="total_price"><h2>Rp<span id="total-price"></h2></span>
+    </div>
+    <table id="detail-grid" class="table table-striped" style="padding: 0px;">
+        <thead style="display: none">
+            <tr>
+                <td style="width: 50px">
+                    <a data-action="delete" title="Delete" href="#"><span class="glyphicon glyphicon-trash"></span></a>
+                    <input type="hidden" data-field="price"><input type="hidden" data-field="id_uom">
+                    <input type="hidden" data-field="id_product">
+                </td>
+                <td>
+                    <ul class="nav nav-list">
+                        <li class="item">
+                            <span data-text="nm_product"></span>
+                        </li>
+                        <li class="qty">
+                            Jumlah <input type="text" size="5" data-field="qty" value="1"> <span data-text="nm_uom"></span>
+                            @ Rp<span data-text="price"></span>
+                        </li>
+                        <li class="discon">
+                            Discon <input type="text" size="5" data-field="discon"> %
 
-						</li>
-					</ul>
-				</td>
-				<td class="total-price">
-					<input type="hidden" data-field="total_price"><span data-text="total_price"></span>
-				</td>
-			</tr>
-		</thead>
-		<tbody>
-		</tbody>
-	</table>
+                        </li>
+                    </ul>
+                </td>
+                <td class="total-price">
+                    <input type="hidden" data-field="total_price"><span data-text="total_price"></span>
+                </td>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
 </div>
