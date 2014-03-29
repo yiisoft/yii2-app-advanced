16950e7835de4adc3ba89cbe2a17c6d0a9659831
samsung-pc <samsung-pc@SangkilSoft>
2014-03-29 20:16:01 +0700

    fixing data schemas

--- backend/modules/master/models/Uom.php
+++ backend/modules/master/models/Uom.php
@@ -2,6 +2,8 @@
 
 namespace backend\modules\master\models;
 
+use Yii;
+
 /**
  * This is the model class for table "uom".
  *
@@ -13,64 +15,122 @@ namespace backend\modules\master\models;
  * @property string $update_date
  * @property integer $update_by
  *
+ * @property SalesDtl[] $salesDtls
+ * @property PurchaseDtl[] $purchaseDtls
+ * @property Cogs[] $cogs
+ * @property StockAdjusmentDtl $stockAdjusmentDtl
  * @property ProductUom[] $productUoms
+ * @property ProductStock[] $productStocks
+ * @property TransferDtl[] $transferDtls
+ * @property Price[] $prices
+ * @property StockOpnameDtl $stockOpnameDtl
  */
-class Uom extends \yii\db\ActiveRecord
-{
-	/**
-	 * @inheritdoc
-	 */
-	public static function tableName()
-	{
-		return 'uom';
-	}
+class Uom extends \yii\db\ActiveRecord {
 
-	/**
-	 * @inheritdoc
-	 */
-	public function rules()
-	{
-		return [
-			[['cd_uom', 'nm_uom'], 'required'],
-			[['cd_uom'], 'string', 'max' => 4],
-			[['nm_uom'], 'string', 'max' => 32],
-			[['cd_uom'], 'unique']
-		];
-	}
+    /**
+     * @inheritdoc
+     */
+    public static function tableName() {
+        return 'uom';
+    }
 
-	/**
-	 * @inheritdoc
-	 */
-	public function attributeLabels()
-	{
-		return [
-			'id_uom' => 'Id Uom',
-			'cd_uom' => 'Cd Uom',
-			'nm_uom' => 'Nm Uom',
-			'create_date' => 'Create Date',
-			'create_by' => 'Create By',
-			'update_date' => 'Update Date',
-			'update_by' => 'Update By',
-		];
-	}
+    /**
+     * @inheritdoc
+     */
+    public function rules() {
+        return [
+            [['cd_uom', 'nm_uom'], 'required'],
+            [['create_date', 'update_date'], 'string'],
+            [['create_by', 'update_by'], 'integer'],
+            [['cd_uom'], 'string', 'max' => 4],
+            [['nm_uom'], 'string', 'max' => 32],
+            [['cd_uom'], 'unique']
+        ];
+    }
 
-	/**
-	 * @return \yii\db\ActiveRelation
-	 */
-	public function getProductUoms()
-	{
-		return $this->hasMany(ProductUom::className(), ['id_uom' => 'id_uom']);
-	}
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels() {
+        return [
+            'id_uom' => 'Id Uom',
+            'cd_uom' => 'Cd Uom',
+            'nm_uom' => 'Nm Uom',
+            'create_date' => 'Create Date',
+            'create_by' => 'Create By',
+            'update_date' => 'Update Date',
+            'update_by' => 'Update By',
+        ];
+    }
 
-	public function behaviors()
-	{
-		return [
-			'timestamp' => [
-				'class' => 'backend\components\AutoTimestamp',
-			],
-			'changeUser' => [
-				'class' => 'backend\components\AutoUser',
-			]
-		];
-	}
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSalesDtls() {
+        return $this->hasMany(SalesDtl::className(), ['id_uom' => 'id_uom']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPurchaseDtls() {
+        return $this->hasMany(PurchaseDtl::className(), ['id_uom' => 'id_uom']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCogs() {
+        return $this->hasMany(Cogs::className(), ['id_uom' => 'id_uom']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStockAdjusmentDtl() {
+        return $this->hasOne(StockAdjusmentDtl::className(), ['id_uom' => 'id_uom']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProductUoms() {
+        return $this->hasMany(ProductUom::className(), ['id_uom' => 'id_uom']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProductStocks() {
+        return $this->hasMany(ProductStock::className(), ['id_uom' => 'id_uom']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTransferDtls() {
+        return $this->hasMany(TransferDtl::className(), ['id_uom' => 'id_uom']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPrices() {
+        return $this->hasMany(Price::className(), ['id_uom' => 'id_uom']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getStockOpnameDtl() {
+        return $this->hasOne(StockOpnameDtl::className(), ['id_uom' => 'id_uom']);
+    }
+
+    public function behaviors() {
+        return [
+            'backend\components\AutoTimestamp',
+            'backend\components\AutoUser'
+        ];
+    }
+
 }
