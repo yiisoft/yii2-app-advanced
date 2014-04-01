e357896a8f1bb584017e50979a862f93d768f17a
samsung-pc <samsung-pc@SangkilSoft>
2014-04-01 11:54:11 +0700

    fixing default sales header value

--- backend/modules/purchase/views/purchase/index.php
+++ backend/modules/purchase/views/purchase/index.php
@@ -14,54 +14,56 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="purchase-hdr-index">
 
-	<h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->title) ?></h1>
 
-	<p>
-		<?= Html::a('Create Purchase Hdr', ['create'], ['class' => 'btn btn-success']) ?>
-	</p>
+    <p>
+        <?= Html::a('Create Purchase Hdr', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
 
-	<?php yii\widgets\Pjax::begin(['formSelector' => 'form', 'enablePushState' => false]); ?>
-	<?php
-	echo GridView::widget([
-		'dataProvider' => $dataProvider,
-		'filterModel' => $searchModel,
-		'columns' => [
-			['class' => 'yii\grid\SerialColumn'],
-			'purchase_num',
-			'idSupplier.nm_supplier',
-			'idBranch.nm_branch',
-			'purchase_date',
-			'nmStatus',
-			[
-				'class' => 'yii\grid\ActionColumn',
-				'template' => '{view} {update} {delete} {receive}',
-				'buttons' => [
-					'update' => function ($url, $model) {
-						return $model->status == PurchaseHdr::STATUS_DRAFT ? Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
-									'title' => Yii::t('yii', 'Update'),
-									'data-pjax' => '0',
-								]) : '';
-					},
-					'delete' => function ($url, $model) {
-						return $model->status == PurchaseHdr::STATUS_DRAFT ? Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
-									'title' => Yii::t('yii', 'Delete'),
-									'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-									'data-method' => 'post',
-									'data-pjax' => '0',
-								]) : '';
-					},
-					'receive' => function ($url, $model) {
-						return $model->status == PurchaseHdr::STATUS_DRAFT ? Html::a('<span class="glyphicon glyphicon-save"></span>', $url, [
-									'title' => Yii::t('yii', 'Receive'),
-									'data-confirm' => Yii::t('yii', 'Are you sure you want to Receive this item?'),
-									'data-method' => 'post',
-									'data-pjax' => '0',
-								]) : '';
-					}
-				]
-			],
-		],
-	]);
-	?>
-	<?php yii\widgets\Pjax::end(); ?>
+    <?php yii\widgets\Pjax::begin(['formSelector' => 'form', 'enablePushState' => false]); ?>
+    <?php
+    echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'tableOptions' => ['class' => 'table table-striped'],
+        'layout' => '{items}{pager}',
+        //'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'purchase_num',
+            'idSupplier.nm_supplier',
+            'idBranch.nm_branch',
+            'purchase_date',
+            'nmStatus',
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{view} {update} {delete} {receive}',
+                'buttons' => [
+                    'update' => function ($url, $model) {
+                    return $model->status == PurchaseHdr::STATUS_DRAFT ? Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, [
+                            'title' => Yii::t('yii', 'Update'),
+                            'data-pjax' => '0',
+                        ]) : '';
+                },
+                    'delete' => function ($url, $model) {
+                    return $model->status == PurchaseHdr::STATUS_DRAFT ? Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+                            'title' => Yii::t('yii', 'Delete'),
+                            'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                            'data-method' => 'post',
+                            'data-pjax' => '0',
+                        ]) : '';
+                },
+                    'receive' => function ($url, $model) {
+                    return $model->status == PurchaseHdr::STATUS_DRAFT ? Html::a('<span class="glyphicon glyphicon-save"></span>', $url, [
+                            'title' => Yii::t('yii', 'Receive'),
+                            'data-confirm' => Yii::t('yii', 'Are you sure you want to Receive this item?'),
+                            'data-method' => 'post',
+                            'data-pjax' => '0',
+                        ]) : '';
+                }
+                ]
+            ],
+        ],
+    ]);
+    ?>
+    <?php yii\widgets\Pjax::end(); ?>
 </div>
