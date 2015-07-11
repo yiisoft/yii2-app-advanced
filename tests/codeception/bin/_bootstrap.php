f8f2ddfbefa21649c624ceabe849b10e42cc1307
Carsten Brandt <mail@cebe.cc>
2015-07-11 18:06:59 +0200

    removed duplicate declares
    these are now defined automatically by console application if needed.
    
    see https://github.com/yiisoft/yii2/issues/6853

--- tests/codeception/bin/_bootstrap.php
+++ tests/codeception/bin/_bootstrap.php
@@ -10,10 +10,6 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'test');
 
-// fcgi doesn't have STDIN and STDOUT defined by default
-defined('STDIN') or define('STDIN', fopen('php://stdin', 'r'));
-defined('STDOUT') or define('STDOUT', fopen('php://stdout', 'w'));
-
 defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', dirname(dirname(dirname(__DIR__))));
 
 require_once(YII_APP_BASE_PATH . '/vendor/autoload.php');
