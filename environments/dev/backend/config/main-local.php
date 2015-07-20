67663dfe1cef4c3b7aab522f436924a2bd059238
Alexander Makarov <sam@rmcreative.ru>
2015-07-21 00:56:46 +0300

    More novice friendly config for gii/debug

--- environments/dev/backend/config/main-local.php
+++ environments/dev/backend/config/main-local.php
@@ -12,10 +12,14 @@ $config = [
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
 
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
 }
 
 return $config;
