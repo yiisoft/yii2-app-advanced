3e48d2930928e8f09ab99a3e89a1b4fba6115af8
Yaroslav Shapoval <shapoval.yaroslav@gmail.com>
2015-08-15 12:15:54 +0300

    Add nowrap only for ActionColumn

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,3 +3,9 @@ Yii::setAlias('common', dirname(__DIR__));
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
+
+\Yii::$container->set('yii\grid\ActionColumn', [
+    'contentOptions' => [
+        'class' => 'text-nowrap'
+    ]
+]);
