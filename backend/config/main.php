cc02669d712cabce2cd517cd376370c3ec9f7ca7
Alexander Makarov <sam@rmcreative.ru>
2015-09-08 01:33:29 +0300

    Added commented urlManager configuration example to config file

--- backend/config/main.php
+++ backend/config/main.php
@@ -29,6 +29,14 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
     ],
     'params' => $params,
 ];
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -28,6 +28,14 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
+        ],
+        */
     ],
     'params' => $params,
 ];
