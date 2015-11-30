77c11117ee0eb2f9f04ceb916915a8f741ad5575
Alexey Rogachev <arogachev90@gmail.com>
2015-11-30 15:07:21 +0600

    Refactored sendEmail() method of PasswordResetRequestForm

--- frontend/models/PasswordResetRequestForm.php
+++ frontend/models/PasswordResetRequestForm.php
@@ -41,20 +41,30 @@ class PasswordResetRequestForm extends Model
             'email' => $this->email,
         ]);
 
-        if ($user) {
-            if (!User::isPasswordResetTokenValid($user->password_reset_token)) {
-                $user->generatePasswordResetToken();
-            }
-
-            if ($user->save()) {
-                return \Yii::$app->mailer->compose(['html' => 'passwordResetToken-html', 'text' => 'passwordResetToken-text'], ['user' => $user])
-                    ->setFrom([\Yii::$app->params['supportEmail'] => \Yii::$app->name . ' robot'])
-                    ->setTo($this->email)
-                    ->setSubject('Password reset for ' . \Yii::$app->name)
-                    ->send();
-            }
+        if (!$user) {
+            return false;
+        }
+        
+        if (!User::isPasswordResetTokenValid($user->password_reset_token)) {
+            $user->generatePasswordResetToken();
+        }
+        
+        if (!$user->save()) {
+            return false;
         }
 
-        return false;
+        return \Yii::$app
+            ->mailer
+            ->compose(
+                [
+                    'html' => 'passwordResetToken-html',
+                    'text' => 'passwordResetToken-text',
+                ],
+                ['user' => $user]
+            )
+            ->setFrom([\Yii::$app->params['supportEmail'] => \Yii::$app->name . ' robot'])
+            ->setTo($this->email)
+            ->setSubject('Password reset for ' . \Yii::$app->name)
+            ->send();
     }
 }
