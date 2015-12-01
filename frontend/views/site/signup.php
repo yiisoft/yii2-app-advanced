645d46b0375500b87ce6293eeee5c771a3fb87d5
MaximAL <almaximal@ya.ru>
2015-12-01 20:14:40 +0300

    Autofocus on the first form field
    Useful UX improvement.

--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -19,7 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <div class="col-lg-5">
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
-                <?= $form->field($model, 'username') ?>
+                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
                 <?= $form->field($model, 'email') ?>
 
