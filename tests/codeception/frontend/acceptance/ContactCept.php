5e0b3cd6a53f3dff8db85ba8eaa1aa321e42644b
Edo Freriks <edofre@gmail.com>
2016-01-31 01:19:07 +0100

    #115, typo fix codeception

--- tests/codeception/frontend/acceptance/ContactCept.php
+++ tests/codeception/frontend/acceptance/ContactCept.php
@@ -35,7 +35,7 @@ $contactPage->submit([
 if (method_exists($I, 'wait')) {
     $I->wait(3); // only for selenium
 }
-$I->expectTo('see that email adress is wrong');
+$I->expectTo('see that email address is wrong');
 $I->dontSee('Name cannot be blank', '.help-block');
 $I->see('Email is not a valid email address.', '.help-block');
 $I->dontSee('Subject cannot be blank', '.help-block');
