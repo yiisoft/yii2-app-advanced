1f08885d5c6cce690fb00d3fafa78a1c3d841675
Oleksandr Roslov <tr@dupkiller.net>
2016-03-16 16:49:23 +0200

    Created environment-based config for tests. Fixes #133

--- tests/codeception/config/frontend/functional.php
+++ tests/codeception/config/frontend/functional.php
@@ -11,6 +11,7 @@ return yii\helpers\ArrayHelper::merge(
     require(YII_APP_BASE_PATH . '/frontend/config/main.php'),
     require(YII_APP_BASE_PATH . '/frontend/config/main-local.php'),
     require(dirname(__DIR__) . '/config.php'),
+    require(dirname(__DIR__) . '/config-local.php'),
     require(dirname(__DIR__) . '/functional.php'),
     require(__DIR__ . '/config.php'),
     [
