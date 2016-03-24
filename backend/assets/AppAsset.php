2f55518a7bb389cb18227b5bb98519c5c7d95353
Evgeniy Tkachenko <et.coder@gmail.com>
2016-03-24 14:04:57 +0300

    Remove copyright notice

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -1,17 +1,11 @@
 <?php
-/**
- * @link http://www.yiiframework.com/
- * @copyright Copyright (c) 2008 Yii Software LLC
- * @license http://www.yiiframework.com/license/
- */
 
 namespace backend\assets;
 
 use yii\web\AssetBundle;
 
 /**
- * @author Qiang Xue <qiang.xue@gmail.com>
- * @since 2.0
+ * Main backend application asset bundle.
  */
 class AppAsset extends AssetBundle
 {
