23cc5d096a12aec0ef07205fc0a27d34d2ade7a7
Adi Priyanto <adipriyanto.bpn@gmail.com>
2016-04-16 16:29:16 +0700

    fix namespace in docblock

--- tests/codeception/frontend/_pages/SignupPage.php
+++ tests/codeception/frontend/_pages/SignupPage.php
@@ -6,7 +6,7 @@ use \yii\codeception\BasePage;
 
 /**
  * Represents signup page
- * @property \codeception_frontend\AcceptanceTester|\codeception_frontend\FunctionalTester $actor
+ * @property \tests\codeception\frontend\AcceptanceTester|\tests\codeception\frontend\FunctionalTester $actor
  */
 class SignupPage extends BasePage
 {
