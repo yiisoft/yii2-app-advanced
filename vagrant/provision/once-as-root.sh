fec06983278b4f1c821c3d1116b04910aa03328c
Pavel Ivanov <rugabarbo@gmail.com>
2016-04-19 22:37:44 +0300

    Vagrant installation (#64) (#105)
    * Vagrant installation (#64)
    
    * Doc fixes [skip ci]
    
    See #105
    
    * Docs for Windows users [skip ci]
    
    See #105
    
    * More detailed manual
    
    See #105
    
    * Small English grammar fix [skip ci]
    
    See #105
    
    * Vagrant installation improvements (see details below) [skip ci]
    
    1. Permission issues fixed:
    * changed user:owner of synchronized directory to vagrant:vagrant
    * now nginx/php-fpm run by vagrant user
    
    2. Removed unnecessary 'sudo' usages from provision scripts.
    
    3. Configured host name for guest machine console.
    
    4. Added ability of external connection to MySQL (changed my.cnf).
    
    5. Colorized guest machine console.
    
    6. Introduced bash-alias 'app' for 'cd /app' command.
    
    7. Prefix 'yii2-app-advanced' replaced by 'y2aa' for brevity and convenience.
    
    * English hosts file link [skip ci]
    
    * Codeception-ready infrastructure
    
    * Manual divided to Win and *nix sections [skip ci]

---
+++ vagrant/provision/once-as-root.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+#== Import script args ==
+
+timezone=$(echo "$1")
+
+#== Bash helpers ==
+
+function info {
+  echo " "
+  echo "--> $1"
+  echo " "
+}
+
+#== Provision script ==
+
+info "Provision-script user: `whoami`"
+
+info "Allocate swap for MySQL 5.6"
+fallocate -l 2048M /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+
+info "Configure locales"
+update-locale LC_ALL="C"
+dpkg-reconfigure locales
+
+info "Configure timezone"
+echo ${timezone} | tee /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
+
+info "Prepare root password for MySQL"
+debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password password \"''\""
+debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password_again password \"''\""
+echo "Done!"
+
+info "Update OS software"
+apt-get update
+apt-get upgrade -y
+
+info "Install additional software"
+apt-get install -y git php5-curl php5-cli php5-intl php5-mysqlnd php5-gd php5-fpm nginx mysql-server-5.6
+
+info "Configure MySQL"
+sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
+echo "Done!"
+
+info "Configure PHP-FPM"
+sed -i 's/user = www-data/user = vagrant/g' /etc/php5/fpm/pool.d/www.conf
+sed -i 's/group = www-data/group = vagrant/g' /etc/php5/fpm/pool.d/www.conf
+sed -i 's/owner = www-data/owner = vagrant/g' /etc/php5/fpm/pool.d/www.conf
+echo "Done!"
+
+info "Configure NGINX"
+sed -i 's/user www-data/user vagrant/g' /etc/nginx/nginx.conf
+echo "Done!"
+
+info "Enabling site configuration"
+ln -s /app/vagrant/nginx/app.conf /etc/nginx/sites-enabled/app.conf
+echo "Done!"
+
+info "Initailize databases for MySQL"
+mysql -uroot <<< "CREATE DATABASE yii2advanced"
+mysql -uroot <<< "CREATE DATABASE yii2_advanced_tests"
+echo "Done!"
+
+info "Install composer"
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
