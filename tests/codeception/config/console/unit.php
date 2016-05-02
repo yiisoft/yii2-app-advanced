c41cc39e5e754ed96e97cf40703f06accfb4c9ee
Anthony Lyappiev <admin@antavr.ru>
2016-05-02 12:56:51 +0300

    подкорректировал dirs

--- tests/codeception/config/console/unit.php
+++ tests/codeception/config/console/unit.php
@@ -7,9 +7,9 @@ return yii\helpers\ArrayHelper::merge(
     require(YII_APP_BASE_PATH . '/common/config/main-local.php'),
     require(YII_APP_BASE_PATH . '/console/config/main.php'),
     require(YII_APP_BASE_PATH . '/console/config/main-local.php'),
-    require(dirname(__DIR__) . '/config.php'),
-    require(dirname(__DIR__) . '/config-local.php'),
-    require(dirname(__DIR__) . '/unit.php'),
+    require(__DIR__ . '/../config.php'),
+    require(__DIR__ . '/../config-local.php'),
+    require(__DIR__ . '/../unit.php'),
     [
     ]
 );
