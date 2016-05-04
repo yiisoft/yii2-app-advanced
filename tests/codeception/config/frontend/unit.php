38e86a17014a73b3963960cc72ad5454c9d5d2ac
Anthony Lyappiev <admin@antavr.ru>
2016-05-04 15:30:32 +0300

    заменил dirname, на относительные пути

--- tests/codeception/config/frontend/unit.php
+++ tests/codeception/config/frontend/unit.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Application configuration for frontend unit tests
  */
@@ -8,9 +7,9 @@ return yii\helpers\ArrayHelper::merge(
     require(YII_APP_BASE_PATH . '/common/config/main-local.php'),
     require(YII_APP_BASE_PATH . '/frontend/config/main.php'),
     require(YII_APP_BASE_PATH . '/frontend/config/main-local.php'),
-    require(dirname(__DIR__) . '/config.php'),
-    require(dirname(__DIR__) . '/config-local.php'),
-    require(dirname(__DIR__) . '/unit.php'),
+    require(__DIR__ . '/../config.php'),
+    require(__DIR__ . '/../config-local.php'),
+    require(__DIR__ . '/../unit.php'),
     require(__DIR__ . '/config.php'),
     [
     ]
