455564c55df9052ab06a898fdcbcd340cd5a93ed
PAVLO <pasha92@ukr.net>
2016-10-04 11:56:57 +0300

    add rules to urlmanager

--- backend/config/main.php
+++ backend/config/main.php
@@ -46,6 +46,10 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
+                '<_c:(\w|-)+>' => '<_c>/index',
+                '<_c:(\w|-)+>/<id:\d+>'=>'<_c>/view',
+                '<_c:(\w|-)+>/<_a:(\w|-)+>/<id:\d+>'=>'<_c>/<_a>',
+                '<_c:(\w|-)+>/<_a:(\w|-)+>'=>'<_c>/<_a>',
             ],
         ],
 
