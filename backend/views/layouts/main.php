d87e2d4d0b2e5d5ba4c80a225b2e73c2143be4b0
Joel Waldock <joel.c.waldock@gmail.com>
2016-10-13 17:11:09 +1300

    Increase logout button size
    Fixes the logout button by increasing it's size to be the same as other
    links in the navbar.
    
    The logout button size is slightly smaller in the non-collapsed navbar
    as it is made to be the same size using padding on the form. It is also
    smaller in the collapsed navbar where it doesn't take up 100% of the
    width unlike other links.

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -44,7 +44,7 @@ AppAsset::register($this);
             . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
                 'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link']
+                ['class' => 'btn btn-link logout']
             )
             . Html::endForm()
             . '</li>';
