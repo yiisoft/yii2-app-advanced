8dc9afe21951383d1c396497cb9a5db292309bcd
githubjeka <et.coder@gmail.com>
2016-11-12 21:45:30 +0300

    To fix create @webroot

--- backend/config/test.php
+++ backend/config/test.php
@@ -1,4 +1,9 @@
 <?php
 return [
     'id' => 'app-backend-tests',
+    'components' => [
+        'assetManager' => [
+            'basePath' => __DIR__ . '/../web/assets',
+        ],
+    ],
 ];
--- frontend/config/test.php
+++ frontend/config/test.php
@@ -1,4 +1,9 @@
 <?php
 return [
     'id' => 'app-frontend-tests',
+    'components' => [
+        'assetManager' => [
+            'basePath' => __DIR__ . '/../web/assets',
+        ],
+    ],
 ];
