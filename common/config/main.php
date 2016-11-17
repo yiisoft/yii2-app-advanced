a5919c3817e3805095a19165b1af2185b60a2d3e
PAVLO <pasha92@ukr.net>
2016-11-17 11:34:26 +0200

    changes in composer json - post create project command

--- common/config/main.php
+++ common/config/main.php
@@ -25,5 +25,13 @@ return [
         'noty' => [
             'class' => 'lo\modules\noty\Module',
         ],
+        'gridview' =>  [
+            'class' => '\kartik\grid\Module'
+            // enter optional module parameters below - only if you need to
+            // use your own export download action or custom translation
+            // message source
+            // 'downloadAction' => 'gridview/export/download',
+            // 'i18n' => []
+        ],
     ],
 ];
