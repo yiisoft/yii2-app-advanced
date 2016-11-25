84c0869050f6aae9d5c18d789a83948c8c92ac5a
ftlmars <ftlmars@users.noreply.github.com>
2016-11-25 18:47:57 +0200

    minor once-as-vagrant.sh update
    added missing `echo "Done!"`  to end of all iterations once-as-vagrant.sh

--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -22,23 +22,30 @@ echo "Done!"
 
 info "Install plugins for composer"
 composer global require "fxp/composer-asset-plugin:^1.2.0" --no-progress
+echo "Done!"
 
 info "Install codeception"
 composer global require "codeception/codeception=2.0.*" "codeception/specify=*" "codeception/verify=*" --no-progress
 echo 'export PATH=/home/vagrant/.config/composer/vendor/bin:$PATH' | tee -a /home/vagrant/.profile
+echo "Done!"
 
 info "Install project dependencies"
 cd /app
 composer --no-progress --prefer-dist install
+echo "Done!"
 
 info "Init project"
 ./init --env=Development --overwrite=y
+echo "Done!"
 
 info "Apply migrations"
 ./yii migrate <<< "yes"
+echo "Done!"
 
 info "Create bash-alias 'app' for vagrant user"
 echo 'alias app="cd /app"' | tee /home/vagrant/.bash_aliases
+echo "Done!"
 
 info "Enabling colorized prompt for guest console"
 sed -i "s/#force_color_prompt=yes/force_color_prompt=yes/" /home/vagrant/.bashrc
+echo "Done!"
