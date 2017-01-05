007a163b8aa0b079ba341c62e6cd7b8d0944b0f8
Dmitry Kulikov <kulikovdn@gmail.com>
2017-01-05 21:02:42 +0600

    Fix for acceptance test.

--- frontend/tests/acceptance/HomeCest.php
+++ frontend/tests/acceptance/HomeCest.php
@@ -11,8 +11,11 @@ class HomeCest
     {
         $I->amOnPage(Url::toRoute('/site/index'));
         $I->see('My Company');
+
         $I->seeLink('About');
         $I->click('About');
+        $I->wait(2); // wait for page to be opened
+
         $I->see('This is the About page.');
     }
 }
