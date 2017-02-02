58c38545060298bcb631e37bcab42027612ecd4a
bscheshirwork <bscheshir.work@gmail.com>
2017-02-02 17:05:39 +0400

    Use level parameter for dirname() calls instead of multiple calls

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,5 +1,5 @@
 <?php
 Yii::setAlias('@common', dirname(__DIR__));
-Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
-Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
-Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@frontend', dirname(__DIR__, 2) . '/frontend');
+Yii::setAlias('@backend', dirname(__DIR__, 2) . '/backend');
+Yii::setAlias('@console', dirname(__DIR__, 2) . '/console');
