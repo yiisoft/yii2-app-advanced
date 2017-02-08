b01c68157fbbd2e4799cc3475e4fe30591e63517
ElisDN <mail@elisdn.ru>
2017-02-08 21:21:47 +0300

    Removed Vargant deprecated code

--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -23,10 +23,6 @@ echo "Done!"
 info "Install plugins for composer"
 composer global require "fxp/composer-asset-plugin:^1.2.0" --no-progress
 
-info "Install codeception"
-composer global require "codeception/codeception=2.0.*" "codeception/specify=*" "codeception/verify=*" --no-progress
-echo 'export PATH=/home/vagrant/.config/composer/vendor/bin:$PATH' | tee -a /home/vagrant/.profile
-
 info "Install project dependencies"
 cd /app
 composer --no-progress --prefer-dist install
