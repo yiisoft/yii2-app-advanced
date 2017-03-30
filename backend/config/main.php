5b7ba87602711d39db87b441189abd9f3a14452c
anatvas101 <anat-vas101@yandex.ru>
2017-03-30 17:31:29 +0300

    Update main.php
    added kartik gridview module

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,7 +11,16 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+        'gridview' =>  [
+            'class' => '\kartik\grid\Module'
+            // enter optional module parameters below - only if you need to
+            // use your own export download action or custom translation
+            // message source
+            // 'downloadAction' => 'gridview/export/download',
+            // 'i18n' => []
+        ],
+    ],
 
     'layoutPath' => '@app/lte-view/layouts',
 
