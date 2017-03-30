4ba16016a0fdc69a01b37d12f609dbe86a124a92
anatvas101 <anat-vas101@yandex.ru>
2017-03-30 17:32:51 +0300

    Update main.php
    added kartik gridview module

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -14,7 +14,18 @@ return [
 
     'language' => 'ru-RU',
     'sourceLanguage' => 'ru',
-
+    
+    'modules' => [
+        'gridview' =>  [
+            'class' => '\kartik\grid\Module'
+            // enter optional module parameters below - only if you need to
+            // use your own export download action or custom translation
+            // message source
+            // 'downloadAction' => 'gridview/export/download',
+            // 'i18n' => []
+        ],
+    ],
+    
     'components' => [
         'i18n' => [
             'translations' => [
