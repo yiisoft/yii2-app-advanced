6aea4bf964ef7e37b11fb0f7ff4d77089c7d4770
anatvas101 <anat-vas101@yandex.ru>
2017-03-30 17:30:24 +0300

    Update main.php
    deleted common gridview component

--- common/config/main.php
+++ common/config/main.php
@@ -25,13 +25,5 @@ return [
         'noty' => [
             'class' => 'lo\modules\noty\Module',
         ],
-        'gridview' =>  [
-            'class' => '\kartik\grid\Module'
-            // enter optional module parameters below - only if you need to
-            // use your own export download action or custom translation
-            // message source
-            // 'downloadAction' => 'gridview/export/download',
-            // 'i18n' => []
-        ],
     ],
 ];
