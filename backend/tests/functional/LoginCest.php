54df8f68de9aca6057778a6a613a59b126003708
bscheshirwork <bscheshir.work@gmail.com>
2017-04-06 15:05:10 +0300

    cleanup and transaction. #example

--- backend/tests/functional/LoginCest.php
+++ backend/tests/functional/LoginCest.php
@@ -10,15 +10,22 @@ use common\fixtures\UserFixture as UserFixture;
  */
 class LoginCest
 {
-    public function _before(FunctionalTester $I)
-    {
-        $I->haveFixtures([
+    /**
+     * Load fixtures before db transaction begin
+     * Called in _before()
+     * @see \Codeception\Module\Yii2::_before()
+     * @see \Codeception\Module\Yii2::loadFixtures()
+     * @return array
+     */
+    public function _fixtures(){
+        return [
             'user' => [
                 'class' => UserFixture::className(),
                 'dataFile' => codecept_data_dir() . 'login_data.php'
             ]
-        ]);
+        ];
     }
+    
     /**
      * @param FunctionalTester $I
      */
