804ff80de23fd397744af46512330a661d7b07c3
Wade Shuler <wadeshuler@users.noreply.github.com>
2017-04-11 11:16:11 -0400

    add require environment.php

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,4 +1,5 @@
 <?php
+require(__DIR__ . '/../../common/config/environment.php');
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
