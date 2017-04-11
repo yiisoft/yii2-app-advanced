32fc0ada44159a73f6e466116c6411613e4bd96c
Wade Shuler <WadeShuler@users.noreply.github.com>
2017-04-11 09:53:37 -0400

    Force override define
    This should force overriding the constants previously declared in BaseYii

--- environments/dev/common/config/bootstrap.php
+++ environments/dev/common/config/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+define('YII_DEBUG', true);
+define('YII_ENV', 'dev');
 
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
