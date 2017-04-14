923ff525397c975e788fc4b354b9f1a1a785cda7
Brian McGee <brian@mobilejazz.com>
2017-04-14 15:27:59 +0200

    Simplified build script

--- bin/build.sh
+++ bin/build.sh
@@ -6,21 +6,6 @@ ROOT_DIR=$(cd ${SCRIPT_DIR}/..; pwd)
 source ${SCRIPT_DIR}/common.sh
 trap "Exiting" SIGUSR1
 
-if [ "$1" = "--help" ]; then
-    echo "  ./build [mobilejazz_dir] [mobilejazz-contrib_dir]"
-    echo "  [mobilejazz_dir] Optional directory path where all of your mobilejazz projects are checked out."
-    echo "  [mobilejazz-contrib_dir] Optional directory path where all of your mobilejazz-contrib projects are checked out."
-    exit
-fi
-
-docker pull mobilejazz/yii2-docker:apache-php5
-
-export MOBILEJAZZ_DEV=${1-${ROOT_DIR}}
-export MOBILEJAZZ_CONTRIB=${2-${ROOT_DIR}}
-
-echo "MobileJazz projects dir: '$MOBILEJAZZ_DEV'";
-echo "MobileJazz Contrib projects '$MOBILEJAZZ_CONTRIB'";
-
 cd ${ROOT_DIR} && docker-compose up -d --build --remove-orphans
 
 sleep 10    # hack to allow services to start
