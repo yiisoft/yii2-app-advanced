8fd28e3a2d6e5023006968d3e7e4ba3b756f70ed
Brian McGee <brian@mobilejazz.com>
2017-04-14 14:59:18 +0200

    Added component framework configuration

--- src/common/config/params.php
+++ src/common/config/params.php
@@ -3,4 +3,16 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+
+    'cms' => [
+        'components' => [
+            '@common/config/content/components.php'
+        ],
+        'fields' => [
+            '@common/config/content/fields.php'
+        ],
+        'views' => [
+            '@common/config/content/views.php'
+        ]
+    ]
 ];
