7c76d553967c64164011579870670b7c2bc7d4b8
Pashkinz92 <pasha92@ukr.net>
2017-07-06 21:17:12 +0300

    Create test-local.php

---
+++ environments/dev/common/config/test-local.php
@@ -0,0 +1,9 @@
+<?php
+return yii\helpers\ArrayHelper::merge(
+    require(__DIR__ . '/../../common/config/test-local.php'),
+    require(__DIR__ . '/main.php'),
+    require(__DIR__ . '/main-local.php'),
+    require(__DIR__ . '/test.php'),
+    [
+    ]
+);
