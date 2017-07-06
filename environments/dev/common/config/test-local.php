ef61dfffd3ea6b4c5f98855425e4b9fba4705bc6
Pashkinz92 <pasha92@ukr.net>
2017-07-06 21:53:05 +0300

    Update test-local.php

--- environments/dev/common/config/test-local.php
+++ environments/dev/common/config/test-local.php
@@ -4,5 +4,10 @@ return yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/main-local.php'),
     require(__DIR__ . '/test.php'),
     [
+        'components' => [
+            'db' => [
+                'dsn' => 'mysql:host=localhost;dbname=yii2advanced_test',
+            ]
+        ],
     ]
 );
