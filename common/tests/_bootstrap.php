841134d005aa7fcfd7e232165254c71250117567
yyxx9988 <yyxx9988@qq.com>
2017-08-01 10:53:33 +0800

    Use `require` as a statement, not a function

--- common/tests/_bootstrap.php
+++ common/tests/_bootstrap.php
@@ -5,5 +5,5 @@ defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', __DIR__.'/../../');
 
 require_once(__DIR__ .  '/../../vendor/autoload.php');
 require_once(__DIR__ .  '/../../vendor/yiisoft/yii2/Yii.php');
-require(__DIR__ . '/../config/bootstrap.php');
+require __DIR__ . '/../config/bootstrap.php';
 
