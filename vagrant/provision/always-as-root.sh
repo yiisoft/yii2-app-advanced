a37e0a4cde81c81836ae2ee0901d5419b9971d62
Pavel <pashkinz92@gmail.com>
2017-08-10 17:02:47 +0300

    vagrant new config

--- vagrant/provision/always-as-root.sh
+++ vagrant/provision/always-as-root.sh
@@ -13,6 +13,6 @@ function info {
 info "Provision-script user: `whoami`"
 
 info "Restart web-stack"
-service php5-fpm restart
+service php7.0-fpm restart
 service nginx restart
 service mysql restart
--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -20,13 +20,6 @@ info "Configure composer"
 composer config --global github-oauth.github.com ${github_token}
 echo "Done!"
 
-info "Install plugins for composer"
-composer global require "fxp/composer-asset-plugin:~1.1.1" --no-progress
-
-info "Install codeception"
-composer global require "codeception/codeception=2.0.*" "codeception/specify=*" "codeception/verify=*" --no-progress
-echo 'export PATH=/home/vagrant/.config/composer/vendor/bin:$PATH' | tee -a /home/vagrant/.profile
-
 info "Install project dependencies"
 cd /app
 composer --no-progress --prefer-dist install
@@ -35,7 +28,8 @@ info "Init project"
 ./init --env=Development --overwrite=y
 
 info "Apply migrations"
-./yii migrate <<< "yes"
+./yii migrate --interactive=0
+./yii_test migrate --interactive=0
 
 info "Create bash-alias 'app' for vagrant user"
 echo 'alias app="cd /app"' | tee /home/vagrant/.bash_aliases
