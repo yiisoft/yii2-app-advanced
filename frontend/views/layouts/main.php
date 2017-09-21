4ab8ac7d8884e8e959d42c13e8d7cb30ef3475e4
MaximAL <almaximal@ya.ru>
2017-09-21 18:52:33 +0300

    Use application name instead of string literal in the navbar

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -29,7 +29,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
