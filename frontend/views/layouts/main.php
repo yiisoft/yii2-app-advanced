e2a965583989d6279fcba24981ac800f1ce566e1
Achmad Hadi Kurnia <imachmadhadikurnia@gmail.com>
2017-09-30 12:51:40 +0700

    Use application name instead of string literal in the copyright footer line

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -71,7 +71,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; <?= Yii::$app->name ?> <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
