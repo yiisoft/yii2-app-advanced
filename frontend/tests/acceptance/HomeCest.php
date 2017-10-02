6b0de8bd2d733f1887a8bbf8c17da34d4f03b421
Alexander Makarov <sam@rmcreative.ru>
2017-10-02 20:27:57 +0100

    Fixed tests

--- frontend/tests/acceptance/HomeCest.php
+++ frontend/tests/acceptance/HomeCest.php
@@ -9,7 +9,7 @@ class HomeCest
     public function checkHome(AcceptanceTester $I)
     {
         $I->amOnPage(Url::toRoute('/site/index'));
-        $I->see('My Company');
+        $I->see('My Application');
 
         $I->seeLink('About');
         $I->click('About');
--- frontend/tests/functional/HomeCest.php
+++ frontend/tests/functional/HomeCest.php
@@ -9,7 +9,7 @@ class HomeCest
     public function checkOpen(FunctionalTester $I)
     {
         $I->amOnPage(\Yii::$app->homeUrl);
-        $I->see('My Company');
+        $I->see('My Application');
         $I->seeLink('About');
         $I->click('About');
         $I->see('This is the About page.');
