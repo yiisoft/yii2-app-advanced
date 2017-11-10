49bb94acc12333fc7322342373bf9af468236c50
bscheshirwork <bscheshir.work@gmail.com>
2017-11-10 16:10:43 +0300

    move fixtures common to test folder

--- frontend/tests/_bootstrap.php
+++ frontend/tests/_bootstrap.php
@@ -7,3 +7,5 @@ require_once YII_APP_BASE_PATH . '/vendor/autoload.php';
 require_once YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php';
 require_once YII_APP_BASE_PATH . '/common/config/bootstrap.php';
 require_once __DIR__ . '/../config/bootstrap.php';
+
+\Yii::setAlias('@common/fixtures', '@common/tests/_fixtures');
