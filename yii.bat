0d23f40f11999f7a1670384c28555d56db303fa3
PowerGamer1 <PowerGamer1@users.noreply.github.com>
2018-01-05 19:36:46 +0200

    Removed unnecessary copyright comments. [skip ci]

--- yii.bat
+++ yii.bat
@@ -2,11 +2,6 @@
 
 rem -------------------------------------------------------------
 rem  Yii command line bootstrap script for Windows.
-rem
-rem  @author Qiang Xue <qiang.xue@gmail.com>
-rem  @link http://www.yiiframework.com/
-rem  @copyright Copyright (c) 2008 Yii Software LLC
-rem  @license http://www.yiiframework.com/license/
 rem -------------------------------------------------------------
 
 @setlocal
