f9100a0f15e0c4c53db77bac1559d6e29304b0eb
Alexander Makarov <sam@rmcreative.ru>
2018-03-15 01:17:51 +0300

    Register meta tags with a method that works well with page cache

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -19,7 +19,7 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
+    <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
