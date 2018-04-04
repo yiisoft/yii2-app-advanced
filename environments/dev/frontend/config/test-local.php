601241c2a9e874b26a6eacc40a80d8ec455f7145
Alexander Makarov <sam@rmcreative.ru>
2018-04-05 01:53:43 +0300

    Fixes #348: Made test config merging more explicit, fixed priorities

--- environments/dev/frontend/config/test-local.php
+++ environments/dev/frontend/config/test-local.php
@@ -1,9 +1,3 @@
 <?php
-return yii\helpers\ArrayHelper::merge(
-    require __DIR__ . '/../../common/config/test-local.php',
-    require __DIR__ . '/main.php',
-    require __DIR__ . '/main-local.php',
-    require __DIR__ . '/test.php',
-    [
-    ]
-);
+return [
+];
--- environments/dev/frontend/web/index-test.php
+++ environments/dev/frontend/web/index-test.php
@@ -13,6 +13,16 @@ require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../../common/config/bootstrap.php';
 require __DIR__ . '/../config/bootstrap.php';
 
-$config = require __DIR__ . '/../config/test-local.php';
+
+$config = yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/../../common/config/main.php',
+    require __DIR__ . '/../../common/config/main-local.php',
+    require __DIR__ . '/../../common/config/test.php',
+    require __DIR__ . '/../../common/config/test-local.php',
+    require __DIR__ . '/../config/main.php',
+    require __DIR__ . '/../config/main-local.php',
+    require __DIR__ . '/../config/test.php',
+    require __DIR__ . '/../config/test-local.php'
+);
 
 (new yii\web\Application($config))->run();
