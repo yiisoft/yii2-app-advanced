b7f27188ed0bc5835ada2d76176cac37400ddc18
Angel Guevara <angeldelcaos@gmail.com>
2018-07-24 02:17:20 -0500

    Add cookieValidationKey

--- environments/dev/common/config/test-local.php
+++ environments/dev/common/config/test-local.php
@@ -3,6 +3,10 @@ return [
     'components' => [
         'db' => [
             'dsn' => 'mysql:host=localhost;dbname=yii2advanced_test',
-        ]
+        ],
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '',
+        ],
     ],
 ];
