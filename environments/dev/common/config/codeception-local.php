bad64d74e05277d3beec96b6fcaeb80e51cd5e9d
Angel Guevara <angeldelcaos@gmail.com>
2018-07-30 12:15:36 -0500

    separate codeception-local on backend and frontend too

--- environments/dev/common/config/codeception-local.php
+++ environments/dev/common/config/codeception-local.php
@@ -1,12 +1,18 @@
 <?php
-return [
-    'components' => [
-        'db' => [
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced_test',
+
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/main.php',
+    require __DIR__ . '/main-local.php',
+    require __DIR__ . '/test.php',
+    [
+        'components' => [
+            'db' => [
+                'dsn' => 'mysql:host=localhost;dbname=yii2advanced_test',
+            ],
+            'request' => [
+                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                'cookieValidationKey' => '',
+            ],
         ],
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
-        ],
-    ],
-];
+    ]
+);
