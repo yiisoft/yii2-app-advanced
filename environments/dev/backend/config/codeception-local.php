a1ba497932b7989b50331682b832bd5165d22679
Angel Guevara <angeldelcaos@gmail.com>
2018-07-31 00:07:17 -0500

    add codeception-local.php consumes test-local.php

--- environments/dev/backend/config/codeception-local.php
+++ environments/dev/backend/config/codeception-local.php
@@ -5,6 +5,7 @@ return yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/main.php',
     require __DIR__ . '/main-local.php',
     require __DIR__ . '/test.php',
+    require __DIR__ . '/test-local.php',
     [
     ]
 );
--- environments/dev/common/config/codeception-local.php
+++ environments/dev/common/config/codeception-local.php
@@ -4,11 +4,9 @@ return yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/main.php',
     require __DIR__ . '/main-local.php',
     require __DIR__ . '/test.php',
+    require __DIR__ . '/test-local.php',
     [
         'components' => [
-            'db' => [
-                'dsn' => 'mysql:host=localhost;dbname=yii2advanced_test',
-            ],
             'request' => [
                 // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
                 'cookieValidationKey' => '',
