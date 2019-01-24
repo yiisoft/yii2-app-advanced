f29b1899fc8e6c647083905f64210288e25d4803
Rafał Niedźwiecki <rafal.niedzwiecki@wooopit.com>
2019-01-24 11:20:29 +0100

    add migration

---
+++ console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use \yii\db\Migration;
+
+class m190124_110200_add_verification_token_column_to_user_table extends Migration
+{
+    public function up()
+    {
+        $this->addColumn('{{user}}', 'verification_token', $this->string()->defaultValue(null));
+    }
+
+    public function down()
+    {
+        $this->dropColumn('{{user}}', 'verification_token');
+    }
+}
