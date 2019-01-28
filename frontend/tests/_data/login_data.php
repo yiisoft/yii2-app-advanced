ece8dd2b9117caa0beac1aa294bdad7bee788f4a
Rafał Niedźwiecki <rafal.niedzwiecki@wooopit.com>
2019-01-28 14:49:52 +0100

    add new users data

--- frontend/tests/_data/login_data.php
+++ frontend/tests/_data/login_data.php
@@ -10,4 +10,15 @@ return [
         'updated_at' => '1392559490',
         'email' => 'sfriesen@jenkins.info',
     ],
+    [
+        'username' => 'test.test',
+        'auth_key' => 'O87GkY3_UfmMHYkyezZ7QLfmkKNsllzT',
+        // Test1234
+        'password_hash' => 'O87GkY3_UfmMHYkyezZ7QLfmkKNsllzT',
+        'email' => 'test@mail.com',
+        'status' => '9',
+        'created_at' => '1548675330',
+        'updated_at' => '1548675330',
+        'verification_token' => '4ch0qbfhvWwkcuWqjN8SWRq72SOw1KYT_1548675330',
+    ],
 ];
