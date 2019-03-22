8c6b707a6b7f103b34903880170953f8cfd684b8
Alexander Makarov <sam@rmcreative.ru>
2019-03-22 23:47:53 +0300

    Fixed formatting [skip ci]

--- common/mail/emailVerify-html.php
+++ common/mail/emailVerify-html.php
@@ -11,5 +11,5 @@ $verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'to
 
     <p>Follow the link below to verify your email:</p>
 
-    <p><?= Html::a(Html::encode($verifyLink), $verifyLink ) ?></p>
+    <p><?= Html::a(Html::encode($verifyLink), $verifyLink) ?></p>
 </div>
