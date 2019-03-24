c4c4c9fbb8bfff0f15092dc5d3e8b42874f2f2b9
Andrey Leontev <kot.ezhva@gmail.com>
2019-03-24 12:15:16 +0300

    Fix default migration with 'tablePrefix'

--- console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
+++ console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
@@ -6,11 +6,11 @@ class m190124_110200_add_verification_token_column_to_user_table extends Migrati
 {
     public function up()
     {
-        $this->addColumn('{{user}}', 'verification_token', $this->string()->defaultValue(null));
+        $this->addColumn('{{%user}}', 'verification_token', $this->string()->defaultValue(null));
     }
 
     public function down()
     {
-        $this->dropColumn('{{user}}', 'verification_token');
+        $this->dropColumn('{{%user}}', 'verification_token');
     }
 }
