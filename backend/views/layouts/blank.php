dcd4d6d572702020ac714d9f5b8ddcc322ab0b71
Deryabin Sergey <deryabinsergey@gmail.com>
2020-09-01 02:50:05 +0300

    Add minimal center layout and login form (#478)

---
+++ backend/views/layouts/blank.php
@@ -0,0 +1,34 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use backend\assets\AppAsset;
+use yii\helpers\Html;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php $this->registerCsrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body class="blank">
+<?php $this->beginBody() ?>
+
+    <div class="wrap">
+        <?= $content ?>
+    </div>
+
+</div>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
