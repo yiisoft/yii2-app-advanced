6686c4b17a9fe086989c76f65518e20f03ff2aef
Алексей <mrZema@inbox.ru>
2020-11-16 19:14:38 +0700

    Update bootstrap.php

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,3 +3,4 @@ Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
+Yii::setAlias('@core', dirname(dirname(__DIR__)) . '/core');
