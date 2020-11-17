1e3e6cfbbbc75648f5f48ef2f1b6dd70176f0557
Алексей <mrZema@inbox.ru>
2020-11-17 13:29:51 +0700

    Update url_manager.php

--- frontend/config/url_manager.php
+++ frontend/config/url_manager.php
@@ -6,9 +6,11 @@ return [
     'enablePrettyUrl' => true,
     'showScriptName' => false,
     'rules' => [
+        '' => 'site/index',
+        'contact' => 'contact/index',
+        'signup' => 'auth/signup/request',
+        'signup/<_a:[\w-]+>' => 'auth/signup/<_a>',
+        'reset' => 'auth/reset/request',
         '<_a:login|logout>' => 'auth/auth/<_a>',
-
-        'contact' => 'site/contact',
-        '<action>' => '<action>/index',
     ],
 ];
