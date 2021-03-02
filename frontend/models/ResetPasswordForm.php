81998da3ddf623c849824b15a81c818eb18ed4b1
Alexander Makarov <sam@rmcreative.ru>
2021-03-03 01:00:50 +0300

    Invalidate authentication key on password change

--- frontend/models/ResetPasswordForm.php
+++ frontend/models/ResetPasswordForm.php
@@ -59,6 +59,7 @@ class ResetPasswordForm extends Model
         $user = $this->_user;
         $user->setPassword($this->password);
         $user->removePasswordResetToken();
+        $user->generateAuthKey();
 
         return $user->save(false);
     }
