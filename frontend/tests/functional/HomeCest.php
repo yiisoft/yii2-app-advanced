f23168345901ddc30957f699061590db64389089
Luke English <luke@developedsoftware.com>
2022-02-23 17:24:42 +0000

    Compatible with PHP 8.1 and Codeception 5

--- frontend/tests/functional/HomeCest.php
+++ frontend/tests/functional/HomeCest.php
@@ -8,7 +8,7 @@ class HomeCest
 {
     public function checkOpen(FunctionalTester $I)
     {
-        $I->amOnPage(\Yii::$app->homeUrl);
+        $I->amOnRoute(\Yii::$app->homeUrl);
         $I->see('My Application');
         $I->seeLink('About');
         $I->click('About');
