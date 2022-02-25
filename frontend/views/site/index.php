69cf3e9cbd1632ec9bccf281a02c779c8ab36827
themikem <ke5czk@gmail.com>
2022-02-25 08:57:46 -0600

    Update advanced template to Bootstrap 5 & remove deprecated swiftmailer package (#516)
    Co-authored-by: Luke English <luke@developedsoftware.com>

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -5,13 +5,12 @@
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    <div class="p-5 mb-4 bg-transparent rounded-3">
+        <div class="container-fluid py-5 text-center">
+            <h1 class="display-4">Congratulations!</h1>
+            <p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
+            <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+        </div>
     </div>
 
     <div class="body-content">
