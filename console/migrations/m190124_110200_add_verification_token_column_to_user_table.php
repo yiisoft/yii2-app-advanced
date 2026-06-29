b19f59ff1f7aabfbcaac84c948e61c03b283bc69
Wilmer Arambula <terabytesoftw@gmail.com>
2026-06-29 18:09:23 -0400

    Remove strict types declaration and return types from migration classes.

--- console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
+++ console/migrations/m190124_110200_add_verification_token_column_to_user_table.php
@@ -1,17 +1,15 @@
 <?php
 
-declare(strict_types=1);
-
 use yii\db\Migration;
 
 class m190124_110200_add_verification_token_column_to_user_table extends Migration
 {
-    public function safeUp(): void
+    public function safeUp()
     {
         $this->addColumn('{{%user}}', 'verification_token', $this->string()->defaultValue(null));
     }
 
-    public function safeDown(): void
+    public function safeDown()
     {
         $this->dropColumn('{{%user}}', 'verification_token');
     }
